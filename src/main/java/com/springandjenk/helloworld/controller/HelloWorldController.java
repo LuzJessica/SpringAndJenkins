0c43b19c9b06aaab50fa44773474995e8634d2fe
Jessica <jessicaluz@gec.inatel.br>
2020-05-03 21:56:12 -0300

    Adding HelloWorld controlle

---
+++ src/main/java/com/springandjenk/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springandjenk.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "Hello World";
+	}
+	
+	
+	
+
+}
